cfe8d4263e1f86f28673036e1aed697e653cfeda
Adrian <adrianbravo10@hotmail.com>
2024-04-30 21:03:24 -0600

    Funciona el or

---
+++ project/ex_17_or.py
@@ -0,0 +1,17 @@
+from delta import Compiler, Phase
+
+
+source = '''
+10 || 20 || 30
+'''
+
+c = Compiler('program')
+c.realize(source)
+print()
+# print(c.parse_tree_str)
+# print()
+# print(c.symbol_table)
+# print()
+print(c.wat_code)
+print()
+print(c.result)
